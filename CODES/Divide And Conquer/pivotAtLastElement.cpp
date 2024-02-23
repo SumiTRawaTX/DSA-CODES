a22d59a6161a56150760b9a07e0fea99f2d40a76
SumiTRawaTX <sumitRAWAT2648@gmail.com>
2024-02-24 01:10:22 +0530

    quickSort

---
+++ CODES/Divide And Conquer/pivotAtLastElement.cpp
@@ -0,0 +1,20 @@
+    if(start >= end) {
+        return;
+    }
+    
+    int pivot = end;
+    int i = start - 1;
+    int j = start;
+    
+    while(j < pivot) {
+        if(a[j] < a[pivot]) {
+            i++;
+            swap(a[i], a[j]);
+        }
+        j++;
+    }
+    i++;
+    swap(a[i], a[pivot]);
+    quickSort(a, start, i-1);
+    quickSort(a, i+1, end);
+}
