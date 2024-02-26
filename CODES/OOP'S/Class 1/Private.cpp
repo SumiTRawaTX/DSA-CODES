12f131da061eae7b05dd7b3c06b328b44776c90e
SumiTRawaTX <sumitRAWAT2648@gmail.com>
2024-02-26 16:11:48 +0530

    getterSetter

---
+++ CODES/OOP'S/Class 1/Private.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Animals {
+private:
+    int weight;
+
+public:
+    // Getter method
+    int getWeight() {
+        return weight;
+    }
+
+    // Setter method
+    void setWeight(int w) {
+        weight = w;
+    }
+};
+
+int main() {
+    Animals Ramesh;
+    Ramesh.setWeight(101);
+    cout << "Weight: " << Ramesh.getWeight() << endl;
+    return 0;
+}
