7b1cc5be89083bd23f915eee4091de607eb2de40
SumiTRawaTX <sumitRAWAT2648@gmail.com>
2024-02-27 14:56:12 +0530

    1

---
+++ CODES/OOP'S/Class 2/Encapsulation/Example.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Animal {
+    public:
+
+        // data member
+        int age;
+        int weight;
+
+        // member function
+        void eat() {
+            cout << "Eatting" << endl;
+        }
+};
+
+int main () {
+
+
+}
