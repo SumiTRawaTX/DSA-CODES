b922cec10c96c30be902cb1f9e24ab45c067a012
SumiTRawaTX <sumitRAWAT2648@gmail.com>
2024-02-27 19:45:17 +0530

    compilerTimeOverloading

---
+++ CODES/OOP'S/Class 2/Polymorphism/Example2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Maths {
+        public:
+        int sum(int a, int b) {
+            return a+b;
+        }
+        int sum(int a, int b, int c) {
+            return a+b+c;
+        }
+        int sum(int a, float b) {
+            return a+b+100;
+        }
+
+        // compiler time error
+        double sum(int a, int b) {
+            return 500;
+        }
+};
+
+int main() {
+
+    Maths obj;
+    // cout << obj.sum(2,3);
+    // cout << obj.sum(23, 34, 56);  
+    cout << obj.sum(25, 50.55f);
+
+}
