b1702cd366839a03324d0ba2eb0914b95157618a
SumiTRawaTX <sumitRAWAT2648@gmail.com>
2024-03-04 12:36:34 +0530

    simpleLL

---
+++ CODES/Linked List/Class 1/Implementation.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* next;
+
+        Node() {
+            cout << "I am inside default constructor" << endl;
+            this->next = NULL;
+        }
+
+        Node(int data) {
+            cout << "I am inside param constructor" << endl;
+            this->data = data;
+            this->next = NULL;
+        }
+};
+
+int main() {
+
+    // creation of Node
+
+    // node a // static method
+
+    Node *first = new Node(10);
+    Node *second= new Node(30);
+    Node *third = new Node(40);
+    Node *fourth = new Node(60);
+    Node *fifth = new Node(70);
+}
