f464a91ebdea1566dc30ec108edf38e362594088
SumiTRawaTX <sumitRAWAT2648@gmail.com>
2024-03-16 16:17:53 +0530

    hackerRank

---
+++ CODES/Linked List/PracticeQuestion/kthNodeFromTheEnd.cpp
@@ -0,0 +1,17 @@
+void fun(ListNode* head, int& position, int & ans) {
+    if(head == 0) return;
+    fun(head->next, position, ans);
+
+    if(postion ==0) {
+        ans = head->data;
+        
+    }
+
+    position --;
+}
+
+int getNode(ListNode* list, int position) {
+    int ans = -1;
+    fun(list, position, ans);
+    return ans;
+}
