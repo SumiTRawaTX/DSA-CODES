0c2a836c355d5a04b71646621b9ae91d503955a5
SumiTRawaTX <sumitRAWAT2648@gmail.com>
2024-04-06 12:14:50 +0530

    TopView

---
+++ CODES/Tree/Class 3/Example3.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include<queue>
+#include<map>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int val) {
+            this->data = val;
+            this->left = NULL;           
+        }
+};
+
+Node* createTree() {
+    cout << "enter the value for Node: " << endl;
+    int value;
+    cin >> value;
+
+    if(value == -1) return NULL;
+
+    //create
+    Node* root = new Node(value);
+    cout << "entering in left of:" << value << endl;
+    root->left = createTree();
+    cout << "entering in right of:" << value << endl;
+    root->right = createTree();
+
+    return root;
+}
+
+void levelOrderTraversal(Node* root) {
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    cout << endl;
+
+    while(!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL) {
+            cout << endl;
+            if(!q.empty()) {
+                q.push(NULL);
+            }
+        }
+        else {
+            cout << temp->data << " ";
+            if(temp->left != NULL) q.push(temp->left);
+            if(temp->right != NULL) q.push(temp->right);
+        }
+    }
+}
+
+void printTopView(Node* root) {
+    map<int, int> hdToNodemap;
+    queue<pair<Node* , int>> q;
+    q.push(make_pair(root, 0));
+
+    while(!q.empty()) {
+        pair<Node*, int> temp = q.front();
+        q.pop();
+
+        Node* frontNode = temp.first;
+        int hd = temp.second;
+
+        if(hdToNodemap.find(hd) == hdToNodemap.end()) {
+            //if there is no entry for hd in map, then create a new entry
+            hdToNodemap[hd] = frontNode->data;
+        }
+
+        // child ko dekhna hai
+        if(frontNode->left != NULL) {
+            q.push(make_pair(frontNode->left, hd-1));
+        }
+        if(frontNode->right != NULL) {
+            q.push(make_pair(frontNode->right, hd+1));
+        }
+    }
+    cout << "printing top view: " << endl;
+    for(auto i: hdToNodemap) {
+        cout << i.second << " ";
+    }
+}
+
+int main() {
+
+    Node* root = createTree();
+    levelOrderTraversal(root);
+
+    // 10 20 40 -1 -1 50 70 110 -1 -1 111 -1 -1 80 -1 -1 30 -1 60 -1 90 112 -1 -1 113 -1 -1
+
+    printTopView(root);
+
+    return 0;
+}
