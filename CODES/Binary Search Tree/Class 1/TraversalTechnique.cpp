0f92007f6baaa6bc204dbebca9b7b2a7b4ba384a
SumiTRawaTX <sumitRAWAT2648@gmail.com>
2024-04-11 12:31:25 +0530

    preOrder,PostOrder,InOrder

---
+++ CODES/Binary Search Tree/Class 1/TraversalTechnique.cpp
@@ -0,0 +1,126 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        // constructor
+        Node(int data) {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+Node* insertIntoBST(Node* root, int data) {
+    // this is the first node we have to create
+    if(root == NULL) {
+        root = new Node(data);
+        return root;
+    }
+    // no the first node
+    if(root->data > data) {
+        // insert into left
+        root->left = insertIntoBST(root->left, data);
+    }
+    else {
+        // insert into right
+        root->right = insertIntoBST(root->right, data);
+    }
+    return root;
+}
+
+void takeInput(Node* &root) {
+    int data;
+    cin >> data;
+    while(data != -1) {
+        root = insertIntoBST(root, data);
+        cin >> data;
+    }
+}
+
+void levelOrderTraversal(Node* root) {
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()) {
+        Node* front = q.front();
+        q.pop();
+
+        if(front == NULL) {
+            cout << endl;
+
+            if(!q.empty()) 
+                q.push(NULL);
+        }
+        else {
+            cout  << front->data << " ";
+
+            if(front->left != NULL) {
+                q.push(front->left);
+            }
+
+            if(front->right != NULL) {
+                q.push(front->right);
+            }
+        }
+    }
+}
+
+void preOrderTraversal(Node* root) {
+    // nlr
+    if(root == NULL) {
+        return;
+    }
+    cout << root->data << " ";
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+}
+
+void inOrderTraversal(Node* root) {
+    // lnr
+    if(root == NULL) {
+        return;
+    }
+    inOrderTraversal(root->left);
+    cout << root->data << " ";
+    inOrderTraversal(root->right);
+}
+
+void postOrderTraversal(Node* root) {
+    // lrn
+    if(root == NULL) {
+        return;
+    }
+    postOrderTraversal(root->left);
+    postOrderTraversal(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+
+    Node* root = NULL;
+    cout << "enter the value for Node: " << endl;
+    takeInput(root);
+
+    cout << "printing the tree" << endl;
+    levelOrderTraversal(root);
+
+    cout << "printing inOrder - " << endl;
+    inOrderTraversal(root);
+    cout << endl;
+
+    cout << "printing preOrder - " << endl;
+    preOrderTraversal(root);
+    cout << endl;
+
+    cout << "printing postOreder -  " << endl;
+    postOrderTraversal(root);
+    cout << endl;
+
+}
