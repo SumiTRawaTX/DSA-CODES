2c2d59d6b9b8183f45fa13333eb2a27cc6cc0534
SumiTRawaTX <sumitRAWAT2648@gmail.com>
2024-04-21 20:14:28 +0530

    MapOperation

---
+++ CODES/HashMaos&Tries/Class 1/STLoperation.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int main() {
+
+    //creation
+    unordered_map<string, int> m;
+
+    // insertion 1-
+    pair<string, int> p = make_pair("sumit", 1);
+    m.insert(p);
+
+    // insertion 2-
+    pair<string, int> p2("rawat", 2);
+    m.insert(p2);
+
+    // insertion 3-
+    m["singh"] = 3;
+
+
+    // access
+    cout << m.at("rawat") << endl;
+
+    cout << m.at("sumit") << endl;
+
+    cout << m["singh"] << endl;
+
+    // search
+    cout << m.count("inova") << endl;
+    cout << m.count("sumit") << endl;
+
+    if(m.find("fortuner") != m.end()) {
+        cout << "Fortuner is present" << endl;
+    }
+    else {
+        cout << "not present" << endl;
+    }
+
+    //size
+    cout << m.size() << endl;
+
+    cout << m["humour"] << endl;
+    cout << m.size() << endl;
+
+    cout << "printing all entries: " << endl;
+
+    for(auto i: m) {
+        cout << i.first << " ->" << i.second << endl;
+    }
+}
