348ea2fb9c8635cee5d1946cfff00ecbb5a51150
SumiTRawaTX <sumitRAWAT2648@gmail.com>
2024-04-21 20:44:05 +0530

    UsingMap

---
+++ CODES/HashMaos&Tries/Class 1/CountLetter.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int main() {
+    string str = "thriruvananthapuram";
+    unordered_map<char,int> freq;
+
+    for(int i=0; i<str.length(); i++) {
+        char ch = str[i];
+        freq[ch] ++;
+    }
+
+    for(auto i: freq) {
+        cout << i.first << " " << i.second << endl;
+    }
+}
