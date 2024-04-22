c375968750613466279258f29ecaedb5bca3d004
SumiTRawaTX <sumitRAWAT2648@gmail.com>
2024-04-23 00:19:32 +0530

    GFG

---
+++ CODES/HashMaps&Tries/Question Practice/SumEqualToSum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+    int findPairs(long long a[], long long n) {
+        unordered_map<int, bool>map;
+        //pairs
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                int sum = a[i] + a[j];
+                if(map.find(sum) != map.end()) {
+                    return true;
+                }
+                else {
+                    map[sum] = true;
+                }
+            }
+            return false;
+        }
+    }
