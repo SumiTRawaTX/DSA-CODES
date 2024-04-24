eee587e0e02e1c9108e8e6888c036a96c9566ebe
SumiTRawaTX <sumitRAWAT2648@gmail.com>
2024-04-24 23:57:12 +0530

    509

---
+++ CODES/Dynamic Programming/Class 1/FibonacciNumber.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    // recursion solution -
+    int recSolve(int n) {
+        // base case
+        if(n==1 || n == 0) {
+            return n;
+        }
+
+        int ans = recSolve(n-1) + recSolve(n-2);
+        return ans;
+    }
+
+
+    // dp solution using top down approach
+    int topDownSolve(int n, vector<int>& dp) {
+        // base case
+        if(n == 1 || n == 0) return n;
+
+        // step 3 -
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+
+        // step 2 - replace ans with dp
+        dp[n] = topDownSolve(n-1, dp) + topDownSolve(n-2, dp);
+        return dp[n];
+    }
+    int fib(int n) {
+        // int ans = recSolve(n);
+        // return ans;
+
+        // step 1 -
+        vector<int> dp(n+1, -1);
+        int ans = topDownSolve(n, dp);
+        return ans;
+    }
+};
